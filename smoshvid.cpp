516719cc459bb64b6a6768c5274bf7fc37cf8141
Josef Aguilar <me@josefaguilar.com>
2015-10-07 10:21:18 -0500

    Formatting

--- smoshvid.cpp
+++ smoshvid.cpp
@@ -1,13 +1,10 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
-// #include <opencv2/cv.h>
-// #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-// void renderMode(char *chunk, char *duplicate, uchar render);
 
 int main(int argc, char const *argv[])
 {
@@ -17,8 +14,6 @@ int main(int argc, char const *argv[])
     return -1;
   }
 
-  // Mat img, img2;
-  // img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
   VideoCapture cap(argv[1]);
   if (!cap.isOpened()) {
@@ -26,17 +21,9 @@ int main(int argc, char const *argv[])
     return -1;
   }
 
-  //  VideoCapture cap2(argv[1]);
-  // if (!cap2.isOpened()) {
-  //   cout << "!!! Failed to open temporary buffer file: "<< argv[1] << endl;
-  //   return -1;
-  // }
 
-  // VideoWriter ouptput;
-  // ouptput.open("output-video.avi", CV_FOURCC('D', 'I', 'V', 'X', Size(1200, 1600), true));
-  
   VideoWriter output_capture("smoshed.mov",
-    // CV_FOURCC('D', 'I', 'V', 'X'), 
+    // CV_FOURCC('D', 'I', 'V', 'X'), // activate this for non-mac platforms 
     CV_FOURCC('m','p','4', 'v'),
     cap.get(CV_CAP_PROP_FPS),
     cv::Size(cap.get(CV_CAP_PROP_FRAME_WIDTH),
@@ -55,12 +42,12 @@ int main(int argc, char const *argv[])
   for (;;) {
     if (!cap.read(frame))
       break;
-    frame2 = frame.clone();
+    frame2 = frame.clone(); // copy frame to keep color "accurate"
     size_t h = frame.rows;
     size_t w = frame.cols;
     uchar red, green, blue; 
     int value;
-    // cout << "Starting the main loop" << endl;
+
       for (int i = 0; i < h; ++i)
       {
           cv::Vec3b* pixel = frame.ptr<cv::Vec3b>(i); // point to first pixel in row
@@ -100,36 +87,16 @@ int main(int argc, char const *argv[])
           }
       }
 
-    //   for ( int i = 0; i < h; i++) {
-    //       cv::Vec3b* pixel = frame.ptr<cv::Vec3b>(i);
-    //       cv::Vec3b* other = frame2.ptr<cv::Vec3b>(i);
-    //       for ( int j = 0; j < w; j++) {
-    //         pixel[j][2] = other[j][2];
-    //         pixel[j][1] = other[j][1];
-    //         pixel[j][0] = other[j][0];
-    //       }
+    frame2.copyTo(frame); // copy frame back;
+    output_capture.write(frame); // Write to video
 
-    // }
-    frame2.copyTo(frame);
-    output_capture.write(frame);
+    // FREE ALL THE MEMORY!
     frame.release();
     frame2.release();
   }
+
   cap.release();
-  // cap2.release();
   output_capture.release();
-
-  // namedWindow("Original image", WINDOW_AUTOSIZE);
-
-  // imshow("Original image", dupBuffer);
-
-  // waitKey(0);
-  // destroyAllWindows();
-
-  // cout << "Writing image..."<< endl;
-  // if (! imwrite("imagecopy.tif", img)  ) {
-  //   fprintf(stderr, "failed to write image file\n");
-  // }
 
   return 0;
 }
